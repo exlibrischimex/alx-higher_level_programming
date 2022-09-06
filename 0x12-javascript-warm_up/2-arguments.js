5cd12014628f326ebb2873777b5eb569fc6cc4fe
exlibrischimex <exlibrischimex@gmail.com>
2022-09-06 03:59:26 +0100

    Js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,9 +4,9 @@ const { argv } = require('process');
 const argc = argv.length;
 
 if (argc === 2) {
-	console.log('No argument');
+  console.log('No argument');
 } else if (argc === 3) {
-	console.log('Argument found');
-}else {
-	console.log('Arguments found');
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
